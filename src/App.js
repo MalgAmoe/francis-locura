de6c2e1bbfb190fc9af3b4a8098fb9c4aa8bd33c
malgamoe <malgamoe@protonmail.com>
2019-06-06 22:16:52 +0200

    bahh

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="App" onClick={(e) => handleClick(e)}>
       <header className="App-header">
-        <img src={logo} className="francis-locura-logo" alt="logo" />
-        <iframe title="soundcloud" width="33%" height="350" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/686600874&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        <img src={logo} className="francis-locura-logo" />
+        <iframe title="soundcloud" height="350" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/686600874&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
       </header>
     </div>
   );
