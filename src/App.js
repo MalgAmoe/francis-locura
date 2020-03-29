573391e1f10581e1feaf5ba75319bb41c964e4ee
MalgAmoe <malgamoe@protonmail.com>
2020-03-29 11:28:52 +0200

    added youtube

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
         <div style={ {backgroundColor: 'black', borderRadius: 5, padding: 5} }>
           <SocialIcon target="_blank" style={ {margin: 5} } url="https://open.spotify.com/artist/051lPfOQwzIgcYgf6V7KyJ" />
           <SocialIcon target="_blank" style={ {margin: 5} } url="https://www.instagram.com/francislocura/" />
+          <SocialIcon target="_blank" style={ {margin: 5} } url="https://www.youtube.com/channel/UCFT0iO_UsPoOV-k04X8XjDw" />
           <SocialIcon target="_blank" style={ {margin: 5} } url="https://www.facebook.com/Francis-Locura-372140290315918/" />
           <SocialIcon target="_blank" style={ {margin: 5} } url="https://soundcloud.com/francis_locura" />
         </div>
