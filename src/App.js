2660a462126c8f44809593a9e109acc8d765879d
MalgAmoe <malgamoe@protonmail.com>
2020-04-20 00:06:35 +0200

    added bandcamp player

--- src/App.js
+++ src/App.js
@@ -14,8 +14,10 @@ function App() {
   return (
     <div className="App" >
       <header className="App-header">
+        <div style={ {width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 45} }>
+          <iframe style={{border: 0, width: '400px', height: '120px'}} src="https://bandcamp.com/EmbeddedPlayer/album=1585210607/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="http://francislocura.bandcamp.com/album/live-jams">Live Jams by Francis Locura</a></iframe>
+        </div>
         <img onClick={(e) => handleClick(e)} src={logo} className="francis-locura-logo" />
-        <br />
         <div style={ {backgroundColor: 'black', borderRadius: 5, padding: 5} }>
           <SocialIcon target="_blank" style={ {margin: 5} } url="https://open.spotify.com/artist/051lPfOQwzIgcYgf6V7KyJ" />
           <SocialIcon target="_blank" style={ {margin: 5} } url="https://www.instagram.com/francislocura/" />
