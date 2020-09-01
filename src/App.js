34813f98a06a5b52edcc8fecde6e22841e6d775e
MalgAmoe <malgamoe@protonmail.com>
2020-09-01 18:14:19 +0200

    turn logo with mouse

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactGA from 'react-ga';
 import { SocialIcon } from 'react-social-icons';
 import logo from './logo.svg';
@@ -8,21 +8,60 @@ ReactGA.initialize('UA-142131045-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
-  function handleClick(e) {
-    e.preventDefault();
-  }
   return (
     <div className="App" >
       <header className="App-header">
-        <img onClick={(e) => handleClick(e)} src={logo} className="francis-locura-logo" />
+        <VinylLogo />
         <br />
-        <div style={ {backgroundColor: '#F5692C', borderRadius: 5, padding: 5} }>
+        <div style={ {backgroundColor: '#101010', borderRadius: 5, padding: 5} }>
           <SocialIcon target="_blank" style={ {margin: 5} } url="https://open.spotify.com/artist/051lPfOQwzIgcYgf6V7KyJ" />
           <SocialIcon target="_blank" style={ {margin: 5} } url="https://www.instagram.com/francislocura/" />
         </div>
       </header>
     </div>
   );
+}
+
+const VinylLogo = () => {
+  const [rotation, setRotation] = useState(0);
+  const [angle, setAngle] = useState(0);
+  const dragStart = (e) => {
+    e.preventDefault();
+  }
+
+  const rotateLogo = e => {
+    const x = e.nativeEvent.clientX - (e.nativeEvent.target.offsetLeft + e.nativeEvent.target.offsetHeight * 0.5);
+    const y = e.nativeEvent.target.offsetTop + e.nativeEvent.target.offsetWidth * 0.5 - e.nativeEvent.clientY;
+    const pressed = e.nativeEvent.buttons === 1;
+    const newAngle = Math.atan2(y, x) * 180 / Math.PI;
+    if (pressed) {
+      setRotation(rotation + angle - newAngle);
+    }
+    setAngle(newAngle);
+  }
+
+  const moveIn = e => {
+    const x = e.nativeEvent.clientX - (e.nativeEvent.relatedTarget.offsetLeft + e.nativeEvent.relatedTarget.offsetHeight * 0.5);
+    const y = e.nativeEvent.relatedTarget.offsetTop + e.nativeEvent.relatedTarget.offsetWidth * 0.5 - e.nativeEvent.clientY;
+    setAngle(Math.atan2(y, x) * 180 / Math.PI);
+  }
+
+  const moveOut = e => {
+    // console.log(e.nativeEvent);
+    // setRotation(rotation + 90);
+  }
+
+  return (
+    <img
+      onDragStart={dragStart}
+      onMouseMove={rotateLogo}
+      onMouseEnter={moveIn}
+      onMouseLeave={moveOut}
+      src={logo}
+      className="francis-locura-logo"
+      style={{transform: `rotate(${rotation}deg)`}}
+    />
+  )
 }
 
 export default App;
