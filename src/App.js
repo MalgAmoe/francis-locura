ac404f8abeee5bb9f70b1240821897be3686e64c
MalgAmoe <malgamoe@protonmail.com>
2020-09-17 17:02:35 +0200

    add rotation with correct effect

--- src/App.js
+++ src/App.js
@@ -4,99 +4,87 @@ import { SocialIcon } from 'react-social-icons';
 import logo from './logo.svg';
 import './App.css';
 
-ReactGA.initialize('UA-142131045-1');
+ReactGA.initialize('UA-142131045-1', {
+  gaOptions: { cookieFlags: 'max-age=7200;secure;samesite=none'}
+});
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
-    <div className="App" >
-      <header className="App-header">
-        <VinylLogo />
+    <div className='App' >
+      <header className='App-header'>
+        <VinylLogo
+          songTitle='The mega song'
+        />
         <br />
         <div style={ {backgroundColor: '#101010', borderRadius: 5, padding: 5} }>
-          <SocialIcon target="_blank" style={ {margin: 5} } url="https://open.spotify.com/artist/051lPfOQwzIgcYgf6V7KyJ" />
-          <SocialIcon target="_blank" style={ {margin: 5} } url="https://www.instagram.com/francislocura/" />
-          <SocialIcon target="_blank" style={ {margin: 5} } url="https://www.youtube.com/channel/UCFT0iO_UsPoOV-k04X8XjDw" />
+          <SocialIcon target='_blank' style={ {margin: 5} } url='https://open.spotify.com/artist/051lPfOQwzIgcYgf6V7KyJ' />
+          <SocialIcon target='_blank' style={ {margin: 5} } url='https://www.instagram.com/francislocura/' />
+          <SocialIcon target='_blank' style={ {margin: 5} } url='https://www.youtube.com/channel/UCFT0iO_UsPoOV-k04X8XjDw' />
         </div>
       </header>
     </div>
   );
 }
 
-const VinylLogo = () => {
-  const [rotation, setRotation] = useState(0);
-  const [angle, setAngle] = useState(0);
+function useFrameNow(isActive) {
+  const [now, setNow] = useState(0);
+
+  useEffect(() => {
+    if (!isActive) return;
+
+    function tick() {
+      if (!isActive) return;
+      setNow(performance.now());
+      requestAnimationFrame(tick);
+    }
+
+    const id = requestAnimationFrame(tick);
+    return () => cancelAnimationFrame(id);
+  }, [isActive]);
+
+  return now;
+}
+
+const VinylLogo = ({songTitle}) => {
+  const [startTime, setStartTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [pastLapse, setPastLapse] = useState(0);
+
+  const frameNow = useFrameNow(isRunning);
+  const currentLapse = isRunning ? Math.max(0, frameNow - startTime) : 0;
+  const totalLapse = pastLapse + currentLapse;
+  const rotation = (totalLapse * 0.5) % 360;
+
+  function handleClick() {
+    if (isRunning) {
+      setPastLapse(l => l + performance.now() - startTime);
+      setStartTime(null);
+    } else {
+      setStartTime(performance.now());
+    }
+    setIsRunning(!isRunning);
+  }
+
   const dragStart = (e) => {
     e.preventDefault();
   }
 
-  const requestRef = React.useRef();
-  const previousTimerRef = React.useRef();
-
-  const animate = time => {
-    if (previousTimerRef.current !== undefined) {
-      const deltaTime = time - previousTimerRef.current;
-
-      setRotation(prevRotation => (prevRotation + deltaTime * 0.1) % 360);
-    }
-    previousTimerRef.current = time;
-    requestRef.current = requestAnimationFrame(animate);
-  }
-
-  useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const rotateLogo = e => {
-    const x = e.nativeEvent.clientX - (e.nativeEvent.target.offsetLeft + e.nativeEvent.target.offsetHeight * 0.5);
-    const y = e.nativeEvent.target.offsetTop + e.nativeEvent.target.offsetWidth * 0.5 - e.nativeEvent.clientY;
-    const pressed = e.nativeEvent.buttons === 1;
-    const newAngle = Math.atan2(y, x) * 180 / Math.PI;
-    if (pressed) {
-      let newRotation = rotation + angle - newAngle;
-      if (newRotation > 180) newRotation -= 360;
-      else if (newRotation < -180) newRotation += 360;
-      setRotation(newRotation);
-    }
-    setAngle(newAngle);
-  }
-
-  const moveIn = e => {
-    const x = e.nativeEvent.clientX - (e.nativeEvent.relatedTarget.offsetLeft + e.nativeEvent.relatedTarget.offsetHeight * 0.5);
-    const y = e.nativeEvent.relatedTarget.offsetTop + e.nativeEvent.relatedTarget.offsetWidth * 0.5 - e.nativeEvent.clientY;
-    setAngle(Math.atan2(y, x) * 180 / Math.PI);
-  }
-
-  const moveOut = e => {
-    // console.log(e.nativeEvent);
-    // setRotation(rotation + 90);
-  }
-
-  const rotateLogoTouch = e => {
-    const x = e.touches[0].clientX - (e.touches[0].target.offsetLeft + e.touches[0].target.offsetHeight * 0.5);
-    const y = e.touches[0].target.offsetTop + e.touches[0].target.offsetWidth * 0.5 - e.touches[0].clientY;
-    const newAngle = Math.atan2(y, x) * 180 / Math.PI;
-    let newRotation = rotation + angle - newAngle;
-    if (newRotation > 180) newRotation -= 360;
-    else if (newRotation < -180) newRotation += 360;
-    setRotation(newRotation);
-    setAngle(newAngle);
-  }
-
   return (
-    // eslint-disable-next-line jsx-a11y/alt-text
-    <img
-      onDragStart={dragStart}
-      onMouseMove={rotateLogo}
-      onMouseEnter={moveIn}
-      onMouseLeave={moveOut}
-      onTouchMove={rotateLogoTouch}
-      src={logo}
-      className="francis-locura-logo"
-      style={{transform: `rotate(${rotation}deg)`}}
-    />
+    <div>
+        {// eslint-disable-next-line jsx-a11y/alt-text
+        <img
+          onDragStart={dragStart}
+          onClick={handleClick}
+          src={logo}
+          className='francis-locura-logo'
+          style={{transform: `rotate(${rotation}deg)`, cursor: 'pointer'}}
+        />}
+          <h2
+            className='song-title'>
+              {songTitle}
+          </h2>
+      </div>
   )
 }
 
