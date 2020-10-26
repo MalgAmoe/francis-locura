92ae038b3c16b71591cb9441d3848c38ca3cf8de
MalgAmoe <malgamoe@protonmail.com>
2020-10-26 13:44:56 +0100

    remove safari check

--- src/page/App.js
+++ src/page/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
 import { SocialIcon } from 'react-social-icons';
 import { useDispatch, useSelector } from 'react-redux';
@@ -19,7 +19,6 @@ if (process.env.REACT_APP_ENV === 'PROD') {
 export const path = process.env.REACT_APP_SERVER_URL;
 
 function App() {
-  const [isSafari, setIsSafari] = useState(false);
   const selectedSong = useSelector(state => state.selectedSong);
   const songs = useSelector(state => state.songs);
   const audio = useSelector(state => state.audio);
@@ -32,12 +31,7 @@ function App() {
   }
 
   useEffect(() => {
-    const checkIsSafari = navigator.vendor.includes('Apple');
-    if (checkIsSafari) {
-      setIsSafari(true);
-    } else {
       getSongs()(dispatch);
-    }
   }, [dispatch]);
 
   const songTitle = songs[selectedSong] ? songs[selectedSong] : 'Loading...'
@@ -48,8 +42,6 @@ function App() {
 
   return (
     <div className='App' >
-      {isSafari
-        ? <div><p>Safari does not work with this website at the moment.</p><p>If you are using an iOS device, you can't use this website unfortunately. If you're on an Apple device with Mac OS, you can try an other browser and it should work.</p><p>Peace</p></div> :
       <header className='App-header'>
         <ErrorTimer />
         <div className='page-title'></div>
@@ -65,7 +57,6 @@ function App() {
           <SocialIcon target='_blank' style={ {margin: 5} } url='https://www.youtube.com/channel/UCFT0iO_UsPoOV-k04X8XjDw' />
         </div>
       </header>
-      }
     </div>
   );
 }
